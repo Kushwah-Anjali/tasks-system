7d8bfd87ce2fac728299f438d63ea3d36337c2c9
Kushwah-Anjali <anjalikushwah3333@gmail.com>
2026-09-02 14:34:14 +0530

    Add Vercel API proxy

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,9 +4,9 @@ import {
     clearCurrentUser,
 } from "../utils/authStorage";
 
-const api = axios.create({
-    baseURL:
-        import.meta.env.VITE_API_URL,
+
+        const api = axios.create({
+  baseURL: "/api",
     withCredentials: true,
     headers: {
         "Content-Type":
